25d656ec0f84196206f3a840e3c629b7e7094a16
Ceren <cerenaltay.lsb@gmail.com>
2022-04-04 19:38:20 +0300

    action and func delegate example

--- ActionFuncDelegates/Program.cs
+++ ActionFuncDelegates/Program.cs
@@ -2,25 +2,25 @@
 
 namespace ActionFuncDelegates
 {
-class Program
-{
-    public static void Substract(int num1, int num2)
+    class Program
     {
-        Console.WriteLine(num1 - num2);
-    }
+        public static void Subtract(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
 
-    public static int Multiply(int num1, int num2, int num3)
-    {
-        return num1 * num2 * num3;
-    }
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
 
-    static void Main(string[] args)
-    {
-        Action<int, int> substractNumbers = Substract;
-        substractNumbers(10, 3);
+        static void Main(string[] args)
+        {
+            Action<int, int> substractNumbers = Subtract;
+            substractNumbers(10, 3);
 
-        Func<int, int, int, int> multiplyNumbers = Multiply;
-        Console.WriteLine(multiplyNumbers(10, 10, 1));
+            Func<int, int, int, int> multiplyNumbers = Multiply;
+            Console.WriteLine(multiplyNumbers(10, 10, 1));
+        }
     }
-}
 }
